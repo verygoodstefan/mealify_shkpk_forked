b509011dd04d2ca27b27fe3ca0a5a2ef9003baf1
Pratik <pratik@macbook-pro.myfiosgateway.com>
2023-02-15 12:19:48 -0500

    initial setup for ideas page

---
+++ lib/ideas/views/ideas.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Ideas extends StatelessWidget {
+  const Ideas({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(child: Text('Ideas page'));
+  }
+}
