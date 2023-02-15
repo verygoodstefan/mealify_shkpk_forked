c75036582ab29d0792c7391ee8aa24f5b3c92ecf
Pratik <pratik@macbook-pro.myfiosgateway.com>
2023-02-15 13:01:48 -0500

    chore: fix warning

--- lib/ideas/views/ideas.dart
+++ lib/ideas/views/ideas.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Ideas extends StatelessWidget {
-  const Ideas({Key? key}) : super(key: key);
+  const Ideas({super.key});
 
   @override
   Widget build(BuildContext context) {
