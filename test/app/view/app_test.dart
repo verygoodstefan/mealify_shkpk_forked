fc57989735d456d2186d325f2f1f23127e604b46
Pratik <pratik@macbook-pro.myfiosgateway.com>
2023-02-15 13:51:18 -0500

    switch failing test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mealify_shkpk/app/app.dart';
-import 'package:mealify_shkpk/counter/counter.dart';
+import 'package:mealify_shkpk/ideas/views/ideas.dart';
 
 void main() {
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders Ideas Page', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(Ideas), findsOneWidget);
     });
   });
 }
