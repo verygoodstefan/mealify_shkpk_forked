f05ff4d7dbb5a8457a6bcfaef8fe4d25029be19f
Stefan <stefan@verygood.ventures>
2023-02-15 11:53:57 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.mealify_shkpk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:mealify_shkpk/app/app.dart';
+import 'package:mealify_shkpk/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
