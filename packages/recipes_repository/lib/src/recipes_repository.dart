7996c2bc142448d70a55f9d3daa77c85ffe180d6
Stefan <stefan@verygood.ventures>
2023-02-16 16:07:00 -0500

    linter fixes in recipes and api clients

--- packages/recipes_repository/lib/src/recipes_repository.dart
+++ packages/recipes_repository/lib/src/recipes_repository.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:equatable/equatable.dart';
 import 'package:recipes_repository/src/models/pairing.dart';
 import 'package:the_cocktail_db_api_client/the_cocktail_db_api_client.dart';
 import 'package:the_meal_db_api_client/the_meal_db_api_client.dart';
