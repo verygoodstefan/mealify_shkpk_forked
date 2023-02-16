b6cd2e1839785bda86700fe994db530a61047c13
Pratik <pratik@macbook-pro.myfiosgateway.com>
2023-02-16 14:54:35 -0500

    move file under lib folder

---
+++ packages/the_meal_db_api_client/lib/the_meal_db_api_client.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'lib/src/models/meal.dart';
+
+class TheMealDbApiClient {
+  TheMealDbApiClient();
+
+  Future<Meal> getRandomMeal() async {
+    final response =
+        await http.get(Uri.parse('www.themealdb.com/api/json/v1/1/random.php'));
+
+    if (response.statusCode == 200) {
+      return Meal.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    }
+    throw Exception('Request failed');
+  }
+}
