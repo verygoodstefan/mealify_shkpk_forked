c5a701db1b0d084df1baee0349578f86d963095f
Pratik <pratik@macbook-pro.myfiosgateway.com>
2023-02-16 13:09:20 -0500

    remove unncessary try/catch, switch to Meal

--- packages/the_meals_db_api_client/the_meals_db_api_client.dart
+++ packages/the_meals_db_api_client/the_meals_db_api_client.dart
@@ -2,19 +2,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
-import 'libs/src/models/exceptions.dart';
-import 'libs/src/models/meals.dart';
+import 'libs/src/models/meal.dart';
 
 class TheMealDbApiClient {
   TheMealDbApiClient();
 
-  Future<Meals> getRandomMeal() async {
-    try {
-      final response = await http
-          .get(Uri.parse('www.themealdb.com/api/json/v1/1/random.php'));
-      return Meals.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-    } catch (_) {
-      throw CouldNotFetchDetails();
-    }
+  Future<Meal> getRandomMeal() async {
+    final response =
+        await http.get(Uri.parse('www.themealdb.com/api/json/v1/1/random.php'));
+    return Meal.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   }
 }
